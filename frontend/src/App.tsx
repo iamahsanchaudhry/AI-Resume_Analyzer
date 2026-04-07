d5c6c81512f43151df9a0deda2cc6a86188b06e1
Ahsan Javed <ahsanjavedd17@gmail.com>
2026-04-07 23:41:02 +0500

    fix git issues

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-2xl">
+      <Button variant={"outline"}>Click Me!</Button>
+    </div>
+  )
+}
+
+export default App
